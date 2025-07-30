c3f873a0f6e39a1817c524d9d33b46cacc232f86
fercananything <ferdinand.1238073@gmail.com>
2025-07-29 19:27:40 -0700

    PyPI release

--- src/xsnap/_fitting.py
+++ src/xsnap/_fitting.py
@@ -11,6 +11,7 @@ YR_SEC  = 365.25*24*3600           # s
 MSUN_YR  = MSUN_G / YR_SEC     # 1 M⊙/yr  in g/s
 
 # Helper to fit asymmetric powerlaw
+
 def fit_powerlaw_asymmetric(
     x, y, yerr_lo, yerr_hi,
     xerr_lo=None, xerr_hi=None,
